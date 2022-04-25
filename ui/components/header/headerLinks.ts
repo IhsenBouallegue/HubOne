7591c0b37c1b05e0cf93e8ca94caec65c74004f7
Ihsen <bouallegueihsen@gmail.com>
2022-04-25 23:42:27 +0200

    add header

---
+++ ui/components/header/headerLinks.ts
@@ -0,0 +1,11 @@
+import type { HeaderActionProps } from "./Header";
+
+export const headerLinks: HeaderActionProps = {
+  links: [
+    { link: "/", label: "Label" },
+    { link: "/", label: "Label" },
+    { link: "/", label: "Label" },
+  ],
+};
+
+export default headerLinks;
