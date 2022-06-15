5dba08c531c33dfe1e839ef7cf69c28c6856f641
Ihsen <bouallegueihsen@gmail.com>
2022-06-16 00:57:29 +0200

    fix hot reload not working because of wsl2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   experimental: {
     outputStandalone: true,
   },
+  reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
-
-module.exports = nextConfig;
