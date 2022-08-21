6cd18dea55e7a67f55afdb70d5c2a5abf0575e6c
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-08-21 23:52:05 +0200

    feat: add api endpoints

---
+++ pages/api/v1/generalSettings/index.ts
@@ -0,0 +1,61 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../../lib/prisma";
+
+// GET /api/generalSettingss
+async function handleGET(res: NextApiResponse) {
+  try {
+    const items = await prisma.generalSettings.findMany();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
+
+// DELETE /api/generalSettingss
+async function handleDELETE(res: NextApiResponse) {
+  try {
+    const deletedItem = await prisma.generalSettings.deleteMany();
+    res.json(deletedItem);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
+
+// POST /api/generalSettingss
+async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const createdItem = await prisma.generalSettings.create({
+      data: { ...req.body },
+    });
+    res.json(createdItem);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      handleGET(res);
+      break;
+    case "DELETE":
+      handleDELETE(res);
+      break;
+    case "POST":
+      handlePOST(req, res);
+      break;
+    default: // Method Not Allowed
+      res.status(405).end();
+      break;
+  }
+}
+
+export const generalSettings = {
+  api: {
+    externalResolver: true,
+  },
+};
---
+++ pages/api/v1/linkgroups/index.ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../../lib/prisma";
+// GET /api/linkGroups
+async function handleGET(res: NextApiResponse) {
+  try {
+    const items = await prisma.linkGroup.findMany();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
+
+// DELETE /api/linkGroups
+async function handleDELETE(res: NextApiResponse) {
+  try {
+    const deletedItem = await prisma.linkGroup.deleteMany();
+    res.json(deletedItem);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
+
+// POST /api/linkGroups
+async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const createdItem = await prisma.linkGroup.create({
+      data: { ...req.body },
+    });
+    res.json(createdItem);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      handleGET(res);
+      break;
+    case "DELETE":
+      handleDELETE(res);
+      break;
+    case "POST":
+      handlePOST(req, res);
+      break;
+    default: // Method Not Allowed
+      res.status(405).end();
+      break;
+  }
+}
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
