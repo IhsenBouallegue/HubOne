63b102b49bbd8cb39a20d3df87ea432eb7e15bff
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-08-27 01:45:50 +0200

    fix: api base url not found on vercel

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/v1`,
+  baseURL: `${
+    process.env.NEXT_PUBLIC_SERVER_BASE_URL || process.env.VERCEL_URL
+  }/api/v1`,
 });
 
 export default instance;
