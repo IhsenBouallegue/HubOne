0d55f0117941c8180600d58133770948496de1a8
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-10-12 14:33:26 +0200

    fix: make delete icons consistent

--- ui/sections/LinkSection/AccordionControl.tsx
+++ ui/sections/LinkSection/AccordionControl.tsx
@@ -1,5 +1,5 @@
 import type { AccordionControlProps } from "@mantine/core";
-import { Accordion, ActionIcon, Box } from "@mantine/core";
+import { useMantineTheme, Accordion, ActionIcon, Box } from "@mantine/core";
 import { Trash } from "tabler-icons-react";
 
 import { useDelete } from "../../../lib/useQueries";
@@ -10,13 +10,17 @@ export default function AccordionControl({
   ...props
 }: AccordionControlProps & { itemId: number; editMode: boolean }) {
   const deleteItem = useDelete("linkgroups");
-
+  const theme = useMantineTheme();
   return (
     <Box sx={{ display: "flex", alignItems: "center" }}>
       <Accordion.Control {...props} />
       {editMode && (
-        <ActionIcon mx={12}>
-          <Trash strokeWidth={2} onClick={() => deleteItem(itemId)} />
+        <ActionIcon
+          mx={12}
+          color={theme.colors.secondary[4]}
+          onClick={() => deleteItem(itemId)}
+        >
+          <Trash strokeWidth={2} />
         </ActionIcon>
       )}
     </Box>
