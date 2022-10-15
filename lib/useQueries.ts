dfdc8246c70c6da2be2c169b69b32fdeecf12378
Ihsen Bouallegue <48621967+IhsenBouallegue@users.noreply.github.com>
2022-10-16 01:47:46 +0200

    feat: add toasters to show feedback (#45)

--- lib/useQueries.ts
+++ lib/useQueries.ts
@@ -1,4 +1,5 @@
 /* eslint-disable sonarjs/no-duplicate-string */
+import { showNotification } from "@mantine/notifications";
 import type { QueryClient } from "@tanstack/react-query";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
@@ -70,6 +71,15 @@ export function usePost<T>(QUERY_NAME: string) {
       onMutate: defaultMutateList<T>(queryClient, QUERY_NAME),
       onError: (_, __, context) => {
         queryClient.setQueryData([QUERY_NAME], context?.previousItems);
+        showNotification({
+          message: `We couldn't add your ${QUERY_NAME.slice(0, -1)} 😢`,
+          color: "red",
+        });
+      },
+      onSuccess: () => {
+        showNotification({
+          message: `Your new ${QUERY_NAME.slice(0, -1)} has been added! 🥳`,
+        });
       },
       onSettled: () => {
         queryClient.invalidateQueries([QUERY_NAME]);
@@ -82,13 +92,22 @@ export function usePost<T>(QUERY_NAME: string) {
 export function useUpdate<T extends { id: number }>(QUERY_NAME: string) {
   const queryClient = useQueryClient();
   const { mutate } = useMutation(
-    ({ id, ...newItem }: T) => {
-      return axios.patch<T>(`${QUERY_NAME}/${id}`, newItem);
+    (newItem: T) => {
+      return axios.patch<T>(`${QUERY_NAME}/${newItem.id}`, newItem);
     },
     {
       onMutate: defaultMutateItem<T>(queryClient, QUERY_NAME),
       onError: (_, __, context) => {
         queryClient.setQueryData([QUERY_NAME], context?.previousItem);
+        showNotification({
+          message: `We couldn't update your ${QUERY_NAME.slice(0, -1)} 😢`,
+          color: "red",
+        });
+      },
+      onSuccess: () => {
+        showNotification({
+          message: `Your ${QUERY_NAME.slice(0, -1)} has been updated! 🥳`,
+        });
       },
       onSettled: () => {
         queryClient.invalidateQueries([QUERY_NAME]);
@@ -106,7 +125,15 @@ export function useDelete(QUERY_NAME: string) {
     },
     {
       onError: () => {
-        // TODO: handle errors
+        showNotification({
+          message: `The ${QUERY_NAME.slice(0, -1)} couldn't be deleted! 😢`,
+          color: "red",
+        });
+      },
+      onSuccess: () => {
+        showNotification({
+          message: `The ${QUERY_NAME.slice(0, -1)} has been deleted❗`,
+        });
       },
       onSettled: () => {
         queryClient.invalidateQueries([QUERY_NAME]);
@@ -124,7 +151,15 @@ export function useDeleteAll(QUERY_NAME: string) {
     },
     {
       onError: () => {
-        // TODO: handle errors
+        showNotification({
+          message: `The ${QUERY_NAME} couldn't be deleted! 😢`,
+          color: "red",
+        });
+      },
+      onSuccess: () => {
+        showNotification({
+          message: `The ${QUERY_NAME} has been deleted❗`,
+        });
       },
       onSettled: () => {
         queryClient.invalidateQueries([QUERY_NAME]);
