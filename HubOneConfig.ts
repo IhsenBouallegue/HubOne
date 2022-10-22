44397c49b4d174f5f7e1e0236c226d5b7506bd93
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-10-23 01:20:57 +0200

    fix: remove unused file

--- HubOneConfig.ts
+++
@@ -1,8 +0,0 @@
-import type { FooterLink, Hub, Link, LinkGroup } from "@prisma/client";
-
-export interface HubOneConfigType {
-  hub: Hub;
-  links: Link[];
-  linkGroups: LinkGroup[];
-  footerLinks: FooterLink[];
-}
