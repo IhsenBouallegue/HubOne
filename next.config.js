1b5e070d0492b127428480fbd9e26643fe4d3be5
Ihsen Bouallegue <48621967+IhsenBouallegue@users.noreply.github.com>
2022-11-17 00:53:44 +0100

    55 improve pr template (#64)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@
 module.exports = {
   output: "standalone",
   reactStrictMode: true,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
 };
