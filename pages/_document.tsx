ea402ee0c563865985b891063c5e7b4a11d53acb
IhsenBouallegue <bouallegueihsen@gmail.com>
2023-04-11 14:59:16 +0200

    fix: crossOrigin value in fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap"
