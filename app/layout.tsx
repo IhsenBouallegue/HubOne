46fb53b8f48064317572555ca1d08561e80df18b
Ihsen <bouallegueihsen@gmail.com>
2023-09-10 21:22:39 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,18 @@ import "@styles/App.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { Analytics } from "@vercel/analytics/react";
 import { ReactNode } from "react";
 import theme from "theme";
 import Providers from "./providers";
 
-  export const metadata = {
-    title: "HubOne",
-    description: "HubOne",
-  };
+export const metadata = {
+  title: "HubOne",
+  description: "HubOne",
+};
 
-export default function RootLayout({ children }:
-   { children: ReactNode[] }) {
+export default function RootLayout({ children }: { children: ReactNode[] }) {
   return (
-
     <ClerkProvider
       appearance={{
         variables: {
@@ -31,9 +30,9 @@ export default function RootLayout({ children }:
           <ColorSchemeScript forceColorScheme="light" />
         </head>
         <body>
-          
           <MantineProvider theme={theme} forceColorScheme="light">
             <Notifications />
+            <Analytics />
             <Providers>{children}</Providers>
           </MantineProvider>
         </body>
