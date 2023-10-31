0985d9d92ac7148d4767f63b06cb9c05a3131955
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2023-10-31 21:08:20 +0100

    add middleware to route to hubspaces

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
-import { authMiddleware } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next|favicon.ico).*)", "/", "/(api|trpc)(.*)"],
 };
 
-export async function routingMiddleware(req: NextRequest) {
-  const url = req.nextUrl;
+export async function middleware(request: NextRequest) {
+  const url = request.nextUrl;
   const allowedSubdomainRegex = new RegExp(
     `^https?://(\\w+\\.)?(${process.env.NEXT_PUBLIC_ROOT_DOMAIN}|localhost:3000)$`,
     "i"
   );
   const res = NextResponse.next();
-  const origin = req.headers.get("origin");
+  const origin = request.headers.get("origin");
   // allow subdomains to make requests
   if (origin && allowedSubdomainRegex.test(origin)) {
-    res.headers.append("Access-Control-Allow-Origin", origin);
-    res.headers.append(
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("Access-Control-Allow-Origin", origin);
+    requestHeaders.set(
       "Access-Control-Allow-Methods",
       "GET, PATCH, POST, OPTIONS, DELETE, HEAD"
     );
-    res.headers.append(
+    requestHeaders.set(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
@@ -31,31 +31,27 @@ export async function routingMiddleware(req: NextRequest) {
     return res;
   }
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers
+  const hostname = request.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
-
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
-  const path = url.pathname;
-
-  // rewrite root application to `/home` folder
-  if (
-    hostname === "localhost:3000" ||
-    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
-  ) {
-    return NextResponse.rewrite(new URL(`${path}`, req.url));
-  }
-
+  // Get subdomain of request (e.g. demo, www, hubspace1)
   const subdomain = hostname.replace(
     `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
     ""
   );
+  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+  const path = url.pathname;
+
+  // rewrite these subdomains/hostnames to the root domain
+  if (
+    subdomain === "www" ||
+    hostname === "localhost:3000" ||
+    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
+  ) {
+    return NextResponse.rewrite(new URL(`${path}`, request.url));
+  }
+
   return NextResponse.rewrite(
-    new URL(`/hubspaces/${subdomain}${path}`, req.url)
+    new URL(`/hubspaces/${subdomain}${path}`, request.url)
   );
 }
-
-export default authMiddleware({
-  afterAuth: (_auth, req) => routingMiddleware(req),
-  publicRoutes: ["/"],
-});
